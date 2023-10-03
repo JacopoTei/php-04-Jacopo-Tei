9321b4ce5d2851c3f6675075a33b76c8fb35214b
JacopoTei <jacopotei934@gmail.com>
2023-10-03 12:28:29 +0200

    fine esercitazioni

--- risto.php
+++ risto.php
@@ -8,37 +8,33 @@ class Restaurant {
         $this->drinks = $_drinks;
     }
 
-    public function getMenuWithPrices() {
+    public function getMenuWithPrices($dishPrices, $drinkPrices) {
         $menu = [];
 
         foreach ($this->dishes as $dish) {
-            $menu[$dish] = $this->getDishPrice($dish);
+            $menu[$dish] = $this->getDishPrice($dish, $dishPrices);
         }
 
         foreach ($this->drinks as $drink) {
-            $menu[$drink] = $this->getDrinkPrice($drink);
+            $menu[$drink] = $this->getDrinkPrice($drink, $drinkPrices);
         }
 
         return $menu;
     }
 
-    public function getDishPrice($dish) {
-        $dishPrices = [
-            "carbonara" => 10.99,
-            "amatriciana" => 9.99,
-            "gricia" => 11.99,
-        ];
+    public function addDish($dish, $price, &$dishPrices) {
+        $this->dishes[$dish] = $price;
+     
+        $dishPrices[$dish] = $price;
 
+        echo "Ho aggiunto un nuovo piatto ($dish, $price euro) al menu.\n";
+    }
+
+    private function getDishPrice($dish, $dishPrices) {
         return isset($dishPrices[$dish]) ? $dishPrices[$dish] : 0;
     }
 
-    public function getDrinkPrice($drink) {
-        $drinkPrices = [
-            "acqua" => 1.99,
-            "coca" => 2.49,
-            "vino" => 15.99,
-        ];
-
+    private function getDrinkPrice($drink, $drinkPrices) {
         return isset($drinkPrices[$drink]) ? $drinkPrices[$drink] : 0;
     }
 }
@@ -47,10 +43,25 @@ $dishMenu = ["carbonara", "amatriciana", "gricia"];
 $drinksMenu = ["acqua", "coca", "vino"];
 
 $restaurant = new Restaurant($dishMenu, $drinksMenu);
-$menuWithPrices = $restaurant->getMenuWithPrices();
+
+$dishPrices = [
+    "carbonara" => 10.99,
+    "amatriciana" => 9.99,
+    "gricia" => 11.99,
+];
+
+$drinkPrices = [
+    "acqua" => 1.99,
+    "coca" => 2.49,
+    "vino" => 15.99,
+];
+
+$menuWithPrices = $restaurant->getMenuWithPrices($dishPrices, $drinkPrices);
 
 echo "Questo è il nostro menu:\n";
 
 foreach ($menuWithPrices as $item => $price) {
     echo "$item: $price euro\n";
 }
+
+$restaurant->addDish("lasagna", 12.99, $dishPrices);
